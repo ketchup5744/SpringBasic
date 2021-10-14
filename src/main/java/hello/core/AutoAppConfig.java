782f0220f32e5ea9d97a9aaabbf829c61e5dfbc9
Josh <vkd5744@naver.com>
2021-10-14 15:41:52 +0900

    Update 'Component Scan' & 'Auto Dependency injection' System

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 // 기존 예제 코드를 살리기 위해 ComponentScan을 통해 기존 코드들을 무효화 시켜줌
 @ComponentScan(
+        // defalt 값은 @ComponentScan이 붙은 위치의 하위를 모두 탐색함
+        // basePackages = "hello.core.member",
+        // basePackageClasses = AutoAppConfig.class;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
